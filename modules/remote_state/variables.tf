49cb42b75e54ac3ab477db056861f8ea5fea6aee
Mohammud Yassine Jaffoo <yassinejaffoo1@gmail.com>
2024-05-30 11:40:24 +0100

    feat: ensure trivy is ran in correct dir

--- modules/remote_state/variables.tf
+++ modules/remote_state/variables.tf
@@ -1,26 +1,26 @@
 variable "repository" {
   type        = string
-  description = "The name of the repository to lookup remote state for"
+  description = "The name of the repository to lookup remote state for."
 }
 
 variable "reader_role_arn" {
   type        = string
-  description = "The ARN of the reader role to assume in order to read the remote state"
+  description = "The ARN of the reader role to assume in order to read the remote state."
 }
 
 variable "web_identity_token_file" {
   type        = string
-  description = "Path to the web identity token file"
+  description = "Path to the web identity token file."
 }
 
 variable "account_id" {
   type        = string
   default     = null
-  description = "Account ID where the remote state bucket is located"
+  description = "Account ID where the remote state bucket is located."
 }
 
 variable "region" {
   type        = string
   default     = null
-  description = "The region name where the destination resources have been created"
+  description = "The region name where the destination resources have been created."
 }
