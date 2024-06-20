487c6f60df28c0c0fdea7765690a7063334bd58a
mahamed abdullahi <mahamed.abdullahi@appvia.io>
2024-06-20 17:02:12 +0100

    feat: adding the ability to create bitbucket roles

--- modules/provider/locals.tf
+++ modules/provider/locals.tf
@@ -19,6 +19,15 @@ locals {
         "https://gitlab.com",
       ]
     }
+
+    // Public Bitbucket OIDC
+    bitbucket = {
+      name = "Bitbucket"
+      url  = "https://api.bitbucket.org/2.0/workspaces/${var.workspace_name}/pipelines-config/identity/oidc"
+      client_id_list = [
+        "ari:cloud:bitbucket::workspace/${var.workspace_uuid}",
+      ]
+    }
   }
 }
 
--- modules/role/variables.tf
+++ modules/role/variables.tf
@@ -3,6 +3,28 @@ variable "name" {
   description = "Name of the role to create"
 }
 
+variable "workspace_name" {
+  description = "The name of the workspace."
+  type        = string
+  default     = null
+}
+
+variable "workspace_uuid" {
+  description = "Workspace UUID. You can get it in the repository settings in the OpenID connect provider. Don't include the brackets and make sure it is lower cased."
+  type        = string
+  default     = null
+  validation {
+    condition     = var.workspace_uuid == null || can(regex("^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$", var.workspace_uuid))
+    error_message = "The uuid format is not matching. Make sure it is lowercased and brackets are not included. Here's a valid example: 8a1f1c70-cbc0-452c-81ce-07534945e18b."
+  }
+}
+
+variable "repository_uuid" {
+  description = "Repository UUID. You can get it in the repository settings in the OpenID connect provider."
+  type        = string
+  default     = null
+}
+
 variable "description" {
   type        = string
   description = "Description of the role being created"
